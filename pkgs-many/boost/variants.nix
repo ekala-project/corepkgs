d8bc3fa7a22f39328aa67b1144676c4f427e195e
Jonathan Ringer <jonringer117@gmail.com>
2025-12-03 21:24:49 -0800

    boost: adopt mkManyVariant

---
+++ pkgs-many/boost/variants.nix
@@ -0,0 +1,47 @@
+
+{
+  v1_75 = {
+    version = "1.75.0";
+    src-hash = "sha256-lT2zHgFtt7sgfxFDK+998QBRbut0aEP6BIaiIuP9Scs=";
+  };
+  v1_77 = {
+    version = "1.77.0";
+    src-hash = "sha256-/J+F/AMOIzFCkIJBr3qEbmBjCqc4jeml+vsfOiaECFQ=";
+  };
+  v1_78 = {
+    version = "1.78.0";
+    src-hash = "sha256-hoHxddS9smxSIiZleT7vCEkNd1hSkzD5jTsp3Qc1vMw=";
+  };
+  v1_79 = {
+    version = "1.79.0";
+    src-hash = "sha256-R11YnVGn+LO6K6TtoCKxcOViyjt2DukiwUa2xlhW7zk=";
+  };
+  v1_80 = {
+    version = "1.80.0";
+    src-hash = "sha256-HhlWXYLkO8WSCaFo9ayJnTukcdVcdhDGd9TM8snFAMA=";
+  };
+  v1_81 = {
+    version = "1.81.0";
+    src-hash = "sha256-cf7u2QD7zMoEo7Ty+Ep8IXGG8oqUDti37UclmGuvmfo=";
+  };
+  v1_82 = {
+    version = "1.82.0";
+    src-hash = "sha256-puGrmwhg5qKIHdeyH+n3N6CV5fM6OodK/Go0UihZfuY=";
+  };
+  v1_83 = {
+    version = "1.83.0";
+    src-hash = "sha256-ZHjt/i8zBRJ8/+jK9z6gF2xTdp9L8Vhb4jfrMHmMO44=";
+  };
+  v1_84 = {
+    version = "1.84.0";
+    src-hash = "sha256-zEuJOs9kXJ1LaY6aDwjKiEaqXWxoJ1wUw+eUnCQQlFQ=";
+  };
+  v1_85 = {
+    version = "1.85.0";
+    src-hash = "sha256-cAn+H6oWl0dr3HAncDorrbhOhJt7C6rVCGsIe5cfhhc=";
+  };
+  v1_86 = {
+    version = "1.86.0";
+    src-hash = "sha256-G+2I5AQBsst6H3bUurSZ41L6TQxfMcDbrmTiTTTXUTs=";
+  };
+}
