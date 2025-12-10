8b3e6039161843431672c195f0376917f1dbabbc
Jonathan Ringer <jonringer117@gmail.com>
2025-12-09 23:23:46 -0800

    fmt: adopt mkManyVairants

---
+++ pkgs-many/fmt/generic.nix
@@ -0,0 +1,74 @@
+{
+  version,
+  src-hash,
+  patches ? [ ],
+  mkVariantPassthru,
+  ...
+}@variantArgs:
+
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchpatch,
+  cmake,
+  enableShared ? !stdenv.hostPlatform.isStatic,
+
+  # tests
+  mpd ? null,
+  openimageio ? null,
+  fcitx5 ? null,
+  spdlog ? null,
+}@args:
+
+stdenv.mkDerivation {
+  pname = "fmt";
+  inherit version;
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "fmtlib";
+    repo = "fmt";
+    rev = version;
+    hash = src-hash;
+  };
+
+  patches = map (p: fetchpatch p) patches;
+
+  nativeBuildInputs = [ cmake ];
+
+  cmakeFlags = [ (lib.cmakeBool "BUILD_SHARED_LIBS" enableShared) ];
+
+  doCheck = true;
+
+  passthru =
+    (mkVariantPassthru variantArgs args)
+    // {
+      tests = {
+        inherit
+          mpd
+          openimageio
+          fcitx5
+          spdlog
+          ;
+      };
+    };
+
+  meta = with lib; {
+    description = "Small, safe and fast formatting library";
+    longDescription = ''
+      fmt (formerly cppformat) is an open-source formatting library. It can be
+      used as a fast and safe alternative to printf and IOStreams.
+    '';
+    homepage = "https://fmt.dev/";
+    changelog = "https://github.com/fmtlib/fmt/blob/${version}/ChangeLog.rst";
+    downloadPage = "https://github.com/fmtlib/fmt/";
+    maintainers = [ ];
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
+}
