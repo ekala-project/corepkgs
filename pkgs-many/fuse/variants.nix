32ffcbc52de0ee5d16000c5d1e657efaf9723fd3
Jonathan Ringer <jonringer117@gmail.com>
2025-12-10 22:28:47 -0800

    fuse: adopt mkManyVariants

---
+++ pkgs-many/fuse/variants.nix
@@ -0,0 +1,11 @@
+{
+  v2 = rec {
+    version = "2.9.9";
+    src-hash = "sha256-dgjM6M7xk5MHi9xPyCyvF0vq0KM8UCsEYBcMhkrdvfs=";
+  };
+
+  v3 = rec {
+    version = "3.17.4";
+    src-hash = "sha256-G3+cBp8q8S8oLIcgWp0p+TAtzqXlKYbRSY/5Y3L7QO4=";
+  };
+}
