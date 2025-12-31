3f12bfa2ca1a6a329b54f3c0016f135cc5ac4489
Jonathan Ringer <jonringer117@gmail.com>
2025-12-30 19:59:23 -0800

    python3Packages.platformdirs: init at 4.5.0

---
+++ python/pkgs/platformdirs/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatch-vcs,
+  hatchling,
+  pythonOlder,
+}:
+
+buildPythonPackage rec {
+  pname = "platformdirs";
+  version = "4.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tox-dev";
+    repo = "platformdirs";
+    tag = version;
+    hash = "sha256-ESXp9I6SL13BdsQLCUcfS8kwqayURCEguhINnK6Dd6k=";
+  };
+
+  build-system = [
+    hatchling
+    hatch-vcs
+  ];
+
+  pythonImportsCheck = [ "platformdirs" ];
+
+  meta = {
+    description = "Module for determining appropriate platform-specific directories";
+    homepage = "https://platformdirs.readthedocs.io/";
+    changelog = "https://github.com/tox-dev/platformdirs/releases/tag/${src.tag}";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
