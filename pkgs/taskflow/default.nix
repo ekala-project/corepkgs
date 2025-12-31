be550272750eec0a0a34f9976c9492914291337f
Jonathan Ringer <jonringer117@gmail.com>
2025-12-30 19:42:58 -0800

    taskflow: init at 3.11.0

---
+++ pkgs/taskflow/default.nix
@@ -0,0 +1,61 @@
+{
+  cmake,
+  doctest,
+  fetchFromGitHub,
+  lib,
+  replaceVars,
+  stdenv,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "taskflow";
+  version = "3.11.0";
+
+  src = fetchFromGitHub {
+    owner = "taskflow";
+    repo = "taskflow";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-GIP/69JlygdmryRA4uiyXM3FZZ0wkvP3DceYGkn4+QY=";
+  };
+
+  patches = [
+    (replaceVars ./unvendor-doctest.patch {
+      inherit doctest;
+    })
+  ];
+
+  postPatch = ''
+    rm -r 3rd-party
+
+    # tries to use x86 intrinsics on aarch64-darwin
+    sed -i '/^#if __has_include (<immintrin\.h>)/,/^#endif/d' taskflow/utility/os.hpp
+  '';
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  cmakeFlags = [
+    # FIXME remove once Taskflow is updated to 4.0.0
+    (lib.cmakeFeature "CMAKE_CXX_STANDARD" "20")
+    # building the tests implies running them in the buildPhase
+    (lib.cmakeBool "TF_BUILD_TESTS" finalAttrs.finalPackage.doCheck)
+  ];
+
+  # Massive test suite
+  doCheck = false;
+
+  meta = {
+    description = "General-purpose Parallel and Heterogeneous Task Programming System";
+    homepage = "https://taskflow.github.io/";
+    changelog =
+      let
+        release = lib.replaceStrings [ "." ] [ "-" ] finalAttrs.version;
+      in
+      "https://taskflow.github.io/taskflow/release-${release}.html";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
