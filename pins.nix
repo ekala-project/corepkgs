b14df8cbcb6cf498ca1cde433cc611d70a9842dd
Jonathan Ringer <jonringer117@gmail.com>
2026-01-01 12:57:26 -0800

    pins/lib: update

--- pins.nix
+++ pins.nix
@@ -2,7 +2,7 @@
   lib = import (
     builtins.fetchGit {
       url = "https://github.com/ekala-project/nix-lib.git";
-      rev = "0eefb2da00ace95f1128a79b4800f4034ad8ddc0";
+      rev = "2c14377c3f3825e2cd5ebf43858ec4aaac5bffde";
     }
   );
 }
