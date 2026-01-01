4b2fa097d33b1ac318b6d3d39f09b7be9752d727
Jonathan Ringer <jonringer117@gmail.com>
2026-01-01 14:13:22 -0800

    protobuf: init at 33

---
+++ pkgs-many/protobuf/variants.nix
@@ -0,0 +1,34 @@
+{
+  v21 = {
+    version = "21.12";
+    hash = "sha256-VZQEFHq17UsTH5CZZOcJBKiScGV2xPJ/e6gkkVliRCU=";
+  };
+  v25 = {
+    version = "25.8";
+    hash = "sha256-UUUblMKN1xFknFtanq5MRVQ57dzhOV9sPqlw1J37aNE=";
+  };
+  v27 = {
+    version = "27.5";
+    hash = "sha256-wUXvdlz19VYpFGU9o0pap/PrwE2AkopLZJVUqfEpJVI=";
+  };
+  v29 = {
+    version = "29.5";
+    hash = "sha256-nraqBM87DJYU69Zx0dV9OMnI0Jhn8Axg8DvzUQDQ90Y=";
+  };
+  v30 = {
+    version = "30.2";
+    hash = "sha256-kkQQQPU6wk1/UMRwEMLVIR6JB29Fm6L3OrsvaeI6VAA=";
+  };
+  v31 = {
+    version = "31.1";
+    hash = "sha256-E8q8XupOXoCFpXyGNHArfBmVm6ebfDgaJlJyvMqpveU=";
+  };
+  v32 = {
+    version = "32.1";
+    hash = "sha256-wfu1MyCycGpxFB++eicA0F41j886/Y52I/4+ciRUg2o=";
+  };
+  v33 = {
+    version = "33.2";
+    hash = "sha256-SguWBa9VlE15C+eLzcqqusVLgx9kDyPXwYImSE75HCM=";
+  };
+}
