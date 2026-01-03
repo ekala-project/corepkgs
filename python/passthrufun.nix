73a700ec173f80cf6a14b8eed26d9ce32c63c9b2
Jonathan Ringer <jonringer117@gmail.com>
2026-01-03 10:43:27 -0800

    config.overlays.python: init

--- python/passthrufun.nix
+++ python/passthrufun.nix
@@ -91,6 +91,7 @@ let
                 pythonExtension
               ]
               ++ pythonPackagesExtensions
+              ++ config.overlays.python
               ++ [
                 overrides
               ]
