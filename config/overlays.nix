d548242ef6877895a510c5b0b26e70bf33cf8a46
Jonathan Ringer <jonringer117@gmail.com>
2026-01-03 10:43:27 -0800

    config.overlays.python: init

--- config/overlays.nix
+++ config/overlays.nix
@@ -1,4 +1,27 @@
+{ lib, ... }:
+
+let
+  inherit (lib)
+    mkOption
+    types
+    ;
+
+  overlayType = lib.mkOptionType {
+    name = "overlay";
+    description = "overlay";
+    check = lib.isFunction;
+    merge = lib.mergeOneOption;
+  };
+
+in
 {
   options = {
+    overlays.python = mkOption {
+      type = types.listOf overlayType;
+      default = [ ];
+      description = ''
+        Overlays to be applied to each python package set.
+      '';
+    };
   };
 }
