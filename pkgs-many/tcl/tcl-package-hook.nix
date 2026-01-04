077d02110088957a1e4e18b0c3a41a058b869109
Aliaksandr <grubian2@gmail.com>
2026-01-04 23:26:07 +0200

    tcl-package-hook: fix eval

--- pkgs-many/tcl/tcl-package-hook.nix
+++ pkgs-many/tcl/tcl-package-hook.nix
@@ -3,7 +3,4 @@
 makeSetupHook {
   name = "tcl-package-hook";
   propagatedBuildInputs = [ buildPackages.makeBinaryWrapper ];
-  meta = {
-    inherit (meta) maintainers platforms;
-  };
 } ./tcl-package-hook.sh
