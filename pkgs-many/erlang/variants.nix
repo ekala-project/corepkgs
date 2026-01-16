506d55c3571263565c658b01916ed360f3438cd5
Jonathan Ringer <jonringer117@gmail.com>
2026-01-16 13:35:01 -0800

    erlang: init

---
+++ pkgs-many/erlang/variants.nix
@@ -0,0 +1,14 @@
+{
+  v26 = {
+    version = "26.2.5.16";
+    hash = "sha256-3WxURI1o/CqN0KK+voUMk6al2mtsrqoniBJpDxKpxPo=";
+  };
+  v27 = {
+    version = "27.3.4.6";
+    hash = "sha256-QC8C7rOpRXFriZtmVc48Se7IQ61p4DSAXVobnDeEkAE=";
+  };
+  v28 = {
+    version = "28.3";
+    hash = "sha256-Kn6JYy7EIiBT2pTfh8S9uHCsLS9EZlv3NBFxbYmHXJA=";
+  };
+}
