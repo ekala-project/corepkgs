5effb36c97f70883f216e846e8c2472a3dc4d836
Jonathan Ringer <jonringer117@gmail.com>
2026-03-10 07:09:09 -0700

    libtool: adopt mkManyVersions

---
+++ pkgs-many/libtool/variants.nix
@@ -0,0 +1,11 @@
+{
+  v1_5 = {
+    version = "1.5.26";
+    src-hash = "sha256-HDWuNP6FqhZ716tLyfR3/gGROOGvYmeNlS/EPAt+Lwk=";
+  };
+
+  v2_5 = {
+    version = "2.5.4";
+    src-hash = "sha256-2o67LOTc9GuQCY2vliz/po9LT2LqYPeY0O8Skp7eat8=";
+  };
+}
