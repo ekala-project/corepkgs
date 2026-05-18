ab699bc0249ee075c96b7f61b49ea3e1d415b453
Jonathan Ringer <jonringer117@gmail.com>
2026-05-18 09:57:48 -0700

    esbuild: init at 0.28.0

---
+++ pkgs/esbuild/default.nix
@@ -0,0 +1,35 @@
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+}:
+
+buildGoModule rec {
+  pname = "esbuild";
+  version = "0.28.0";
+
+  src = fetchFromGitHub {
+    owner = "evanw";
+    repo = "esbuild";
+    rev = "v${version}";
+    hash = "sha256-unkJ/Zk69wqMqJS3k1ECV3rQiJlAVKVl4F+0hx1nlVs=";
+  };
+
+  vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
+
+  subPackages = [ "cmd/esbuild" ];
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    description = "Extremely fast JavaScript bundler";
+    homepage = "https://esbuild.github.io";
+    changelog = "https://github.com/evanw/esbuild/blob/v${version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "esbuild";
+  };
+}
