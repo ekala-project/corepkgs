4b9a32bdf8d05441130a8c3837214ea14f5f052f
Jonathan Ringer <jonringer117@gmail.com>
2026-05-18 08:00:50 -0700

    sops: init at 3.13.1

---
+++ pkgs/sops/default.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  versionCheckHook,
+  nix-update-script,
+  makeWrapper,
+  runCommand,
+  age,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "sops";
+  version = "3.13.1";
+
+  src = fetchFromGitHub {
+    owner = "getsops";
+    repo = finalAttrs.pname;
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  vendorHash = "sha256-dniVUk/PsF111UCujFY6zIKR1cp8c8ZawH2ywMef9hE=";
+
+  subPackages = [ "cmd/sops" ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/getsops/sops/v3/version.Version=${finalAttrs.version}"
+  ];
+
+  nativeBuildInputs = [
+    installShellFiles
+    makeWrapper
+  ];
+
+  postInstall = ''
+    installShellCompletion --cmd sops --bash ${./bash_autocomplete}
+    installShellCompletion --cmd sops --zsh ${./zsh_autocomplete}
+  '';
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  # wrap sops with age plugins
+  passthru.withAgePlugins =
+    filter:
+    runCommand "sops-${finalAttrs.version}-with-age-plugins"
+      {
+        nativeBuildInputs = [ makeWrapper ];
+      }
+      ''
+        makeWrapper ${lib.getBin finalAttrs.finalPackage}/bin/sops $out/bin/sops \
+          --prefix PATH : "${lib.makeBinPath (filter age.passthru.plugins)}"
+      '';
+
+  meta = {
+    homepage = "https://getsops.io/";
+    description = "Simple and flexible tool for managing secrets";
+    changelog = "https://github.com/getsops/sops/blob/v${finalAttrs.version}/CHANGELOG.md";
+    mainProgram = "sops";
+    maintainers = [ ];
+    license = lib.licenses.mpl20;
+  };
+})
