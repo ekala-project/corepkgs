e045fc65b3f31ad3e7e3030caa24ad1ee0144f38
Jonathan Ringer <jonringer117@gmail.com>
2026-05-18 21:01:45 -0700

    nodejs: ifxup

--- pkgs-many/nodejs/generic.nix
+++ pkgs-many/nodejs/generic.nix
@@ -81,7 +81,8 @@ stdenv.mkDerivation (finalAttrs: {
 
     # Install npm in the same output
     # npm is bundled with Node.js source
-    patchShebangs --host ''${!outputBin}/bin
+    export HOST_PATH="$out/bin:$PATH"
+    patchShebangs --host $out/bin/*
   '';
 
   passthru = {
