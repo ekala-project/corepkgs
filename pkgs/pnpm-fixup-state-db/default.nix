4b981544a84c3fbb2ed55f1363e41efb5f291e74
Jonathan Ringer <jonringer117@gmail.com>
2026-05-25 11:31:32 -0700

    pnpm-fixup-state-db: init at 1.0.0

---
+++ pkgs/pnpm-fixup-state-db/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  nodejs_23,
+  pnpm,
+  tests,
+}:
+
+nodejs_23.buildNpmPackage {
+  pname = "pnpm-fixup-state-db";
+  version = "1.0.0";
+
+  src = ./src;
+
+  npmDepsHash = "sha256-um6a4pEtPtdxHBRq9g5ZW20wIQAMjWJ3qF96XuxJg8o=";
+
+  postInstall = ''
+    makeWrapper ${lib.getExe nodejs_23} $out/bin/pnpm-fixup-state-db \
+      --add-flags "$out/lib/node_modules/pnpm-fixup-state-db"
+  '';
+
+  passthru.tests = {
+    inherit (tests) pnpm;
+  };
+
+  meta = {
+    license = lib.licenses.mit;
+    mainProgram = "pnpm-fixup-state-db";
+    inherit (pnpm.meta) maintainers;
+    description = "Fixup pnpm v11-store index.db to remove impurities";
+  };
+}
