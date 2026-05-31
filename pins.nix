d9c73e7f1235463cde1e1e6a1424c02011f0520d
Jonathan Ringer <jonringer117@gmail.com>
2026-05-31 13:13:53 -0700

    pkgsStatic: fix eval

--- pins.nix
+++ pins.nix
@@ -2,7 +2,7 @@
   lib = import (
     builtins.fetchGit {
       url = "https://github.com/ekala-project/nix-lib.git";
-      rev = "2c14377c3f3825e2cd5ebf43858ec4aaac5bffde";
+      rev = "fd5cdc455e167022c720950fcc599c8a5ef618a1";
     }
   );
 }
