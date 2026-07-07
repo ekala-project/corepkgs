9a6e46da0455c2b2a3cb3a6ef16fbe6bd48d6dc1
Jonathan Ringer <jonringer117@gmail.com>
2026-07-07 11:18:10 -0700

    llhttp: 9.3.0 -> 9.4.2

--- pkgs/llhttp/default.nix
+++ pkgs/llhttp/default.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "llhttp";
-  version = "9.3.0";
+  version = "9.4.2";
 
   src = fetchFromGitHub {
     owner = "nodejs";
     repo = "llhttp";
     tag = "release/v${finalAttrs.version}";
-    hash = "sha256-VL58h8sdJIpzMiWNqTvfp8oITjb0b3X/F8ygaE9cH94=";
+    hash = "sha256-LS8HS8CnXJ3X8WlIvtxBLc0h1wLL/HmTqZWHlvBjTEo=";
   };
 
   outputs = [
