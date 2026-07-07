270eac550cc6cc02d464dc11d430c66722a356b6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-06 21:52:33 -0700

    aws-c-s3: 0.8.7 -> 0.12.7

--- pkgs/aws-c-s3/default.nix
+++ pkgs/aws-c-s3/default.nix
@@ -17,13 +17,13 @@
 stdenv.mkDerivation rec {
   pname = "aws-c-s3";
   # nixpkgs-update: no auto update
-  version = "0.8.7";
+  version = "0.12.7";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-s3";
     rev = "v${version}";
-    hash = "sha256-8yUwgiZ50BiItapeg0zIc5vr0+OFHvzIRrwWH4lQFBM=";
+    hash = "sha256-OEoicLEFbceqjhUNhe/3kVXhj/+ZRQjca3wCmMlbks4=";
   };
 
   nativeBuildInputs = [
