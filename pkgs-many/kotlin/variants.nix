5dc39ac2b642cdba836a431f3985ca4db549bf29
Jonathan Ringer <jonringer117@gmail.com>
2026-07-07 09:55:37 -0700

    kotlin: 2.1.0 -> 2.4.0

--- pkgs-many/kotlin/variants.nix
+++ pkgs-many/kotlin/variants.nix
@@ -1,7 +1,7 @@
 {
   v2_1 = {
-    version = "2.1.0";
+    version = "2.4.0";
     # Kotlin 2.1 (Latest stable with K2 compiler)
-    src-hash = "sha256-tmmNVyitj57c3QFhfWOAcxkdigMTnMU4o5G043Wa0pc=";
+    src-hash = "sha256-uhuebrbdwydQeSJPLp6korAu731Zzi04QE8EsiYTwgo=";
   };
 }
