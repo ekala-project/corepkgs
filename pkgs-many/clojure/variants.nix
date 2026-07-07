49973cece0d53204911ed5e41c05645b415ed1db
Jonathan Ringer <jonringer117@gmail.com>
2026-07-06 19:37:49 -0700

    clj: 1.12.0.1530 -> 1.12.5.1654

--- pkgs-many/clojure/variants.nix
+++ pkgs-many/clojure/variants.nix
@@ -1,7 +1,7 @@
 {
   v1_12 = {
-    version = "1.12.0.1530";
+    version = "1.12.5.1654";
     # Clojure 1.12.0 Tools (Latest stable for 1.12.0)
-    src-hash = "sha256-D/JLioEmujnTLeeEoIdnxd8lk4TLdsbuPbTWECcF7Uk=";
+    src-hash = "sha256-3IbMVrw3L87we9h/RGk+60thz19ENHiDh4Nk0Dtfs0I=";
   };
 }
