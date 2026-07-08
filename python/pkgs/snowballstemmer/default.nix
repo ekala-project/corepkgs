078c439de3d483b28cc426138cd22dcc6f94e37b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-07 18:54:49 -0700

    python312Packages.snowballstemmer: 3.0.1 -> 3.1.1

--- python/pkgs/snowballstemmer/default.nix
+++ python/pkgs/snowballstemmer/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "snowballstemmer";
-  version = "3.0.1";
+  version = "3.1.1";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-bV7u7I6fhNTVa4R2krrPebwsjpDH+AykRE/4tvLlKJU=";
+    sha256 = "sha256-4Hu8VKDXmP5gEKEjmEIuYqi/u6lcOU/QlW71jLTT4mA=";
   };
 
   # No tests included
