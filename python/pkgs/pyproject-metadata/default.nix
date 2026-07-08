fa98a56ba5ea0a541de5db307649fda3b785c224
Jonathan Ringer <jonringer117@gmail.com>
2026-07-07 18:46:05 -0700

    python312Packages.pyproject-metadata: 0.9.1 -> 0.12.1

--- python/pkgs/pyproject-metadata/default.nix
+++ python/pkgs/pyproject-metadata/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "pyproject-metadata";
-  version = "0.9.1";
+  version = "0.12.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "pyproject_metadata";
     inherit version;
-    hash = "sha256-uLIlPdG3Bit4z5SaEV8Cun+kEUqr5j+hBSjp4alUqBY=";
+    hash = "sha256-iAmk32/ggnmzmoiQZpUG7TFY4GF4Vaya/wmPy+dyrkw=";
   };
 
   build-system = [ flit-core ];
