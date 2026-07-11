783d95341a3a4bc7a0544eb362ccfeabe0bc23ab
Jonathan Ringer <jonringer117@gmail.com>
2026-07-11 09:20:18 -0700

    python312Packages.filelock: 3.29.6 -> 3.29.7

--- python/pkgs/filelock/default.nix
+++ python/pkgs/filelock/default.nix
@@ -12,12 +12,12 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "filelock";
-  version = "3.29.6";
+  version = "3.29.7";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-iVxTLvP07wSXK5RGqMTikxpcOZ/zxL5Mk2nyY5uA95M=";
+    hash = "sha256-W0gZeXl65p5y8LOJ2JqAvdWFwmDFs/H7nApbqbs/GV0=";
   };
 
   build-system = [
