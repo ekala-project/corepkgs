c9b92a684fbc6a05eafea75bd41353fb814842db
Jonathan Ringer <jonringer117@gmail.com>
2026-07-15 11:28:30 -0700

    zimg: init at 3.0.6

---
+++ pkgs/zimg/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "zimg";
+  version = "3.0.6";
+
+  src = fetchFromGitHub {
+    owner = "sekrit-twc";
+    repo = "zimg";
+    rev = "release-${finalAttrs.version}";
+    sha256 = "sha256-T+/wuTxPK+PLofqJm3dujGqGGXhpdGQLjAttTQPsgOI=";
+  };
+
+  outputs = [
+    "out"
+    "dev"
+    "doc"
+  ];
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Scaling, colorspace conversion and dithering library";
+    homepage = "https://github.com/sekrit-twc/zimg";
+    license = lib.licenses.wtfpl;
+    platforms = lib.platforms.unix ++ lib.platforms.windows;
+  };
+})
