3cff9e34d4a3039fcee71e98cb1711dc1db70369
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 17:29:50 -0700

    jbig2dec: init at 0.20

---
+++ pkgs/jbig2dec/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  python3,
+  autoconf,
+  automake,
+  libtool,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "jbig2dec";
+  version = "0.20";
+
+  src = fetchurl {
+    url = "https://github.com/ArtifexSoftware/jbig2dec/archive/${finalAttrs.version}/jbig2dec-${finalAttrs.version}.tar.gz";
+    hash = "sha256-qXBTaaZjOrpTJpNFDsgCxWI5fhuCRmLegJ7ekvZ6/yE=";
+  };
+
+  postPatch = ''
+    patchShebangs test_jbig2dec.py
+  '';
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+  ];
+
+  configureScript = "./autogen.sh";
+
+  nativeCheckInputs = [ python3 ];
+
+  meta = {
+    homepage = "https://www.jbig2dec.com/";
+    description = "Decoder implementation of the JBIG2 image compression format";
+    mainProgram = "jbig2dec";
+    license = lib.licenses.agpl3Only;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+})
