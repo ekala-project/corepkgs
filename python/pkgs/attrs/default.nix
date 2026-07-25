29351c6665d8c0a2001f1f98968871ca6f41ad30
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 16:18:08 -0700

    python312Packages.attrs: 25.3.0 -> 26.1.0

--- python/pkgs/attrs/default.nix
+++ python/pkgs/attrs/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "attrs";
-  version = "25.3.0";
+  version = "26.1.0";
   disabled = pythonOlder "3.7";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ddfO/H+1dnR7LIG0RC1NShzgkAlzUnwBHRAw/Tv0rxs=";
+    hash = "sha256-0DzricsyKo/XBtT7kZQHN7ZkKqNpmP4TCpvJbJhe/zI=";
   };
 
   patches = [
@@ -36,7 +36,7 @@ buildPythonPackage rec {
   postInstall = ''
     # Install tests as the tests output.
     mkdir $testout
-    cp -R conftest.py tests $testout
+    cp -R tests $testout
   '';
 
   pythonImportsCheck = [ "attr" ];
