081e58cdd9e1e9105a1e3b71a4d2c00976f311d2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 18:47:10 -0700

    zlib-ng: 2.2.5 -> 2.3.3

--- pkgs/zlib-ng/default.nix
+++ pkgs/zlib-ng/default.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "zlib-ng";
-  version = "2.2.5";
+  version = "2.3.3";
 
   src = fetchFromGitHub {
     owner = "zlib-ng";
     repo = "zlib-ng";
     rev = version;
-    hash = "sha256-c2RYqHi3hj/ViBzJcYWoNib27GAbq/B1SJUfvG7CPG4=";
+    hash = "sha256-6GlHCnx9dQtmViPnvHnMS+l9Z+g6M8ynrSxLhLtmAKU=";
   };
 
   outputs = [
@@ -44,6 +44,7 @@ stdenv.mkDerivation rec {
   cmakeFlags = [
     "-DCMAKE_INSTALL_PREFIX=/"
     "-DBUILD_SHARED_LIBS=ON"
+    "-DBUILD_TESTING=ON"
     "-DINSTALL_UTILS=ON"
   ]
   ++ lib.optionals withZlibCompat [ "-DZLIB_COMPAT=ON" ];
