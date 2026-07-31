97f5ab10e4400dca2faef92a364b0900eb17cdb2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-30 19:14:05 -0700

    python312Packages.pbs-installer: 2026.06.10 -> 2026.07.28

--- python/pkgs/pbs-installer/default.nix
+++ python/pkgs/pbs-installer/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pbs-installer";
-  version = "2026.06.10";
+  version = "2026.07.28";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "frostming";
     repo = "pbs-installer";
     tag = version;
-    hash = "sha256-1hMc6ogMXuTmxaTYmVERhjM9XBp+FgWRqv476vi1pyQ=";
+    hash = "sha256-nQ4RYcdVJGA2DmkT6rJGoW2Svj4r1MQedBIaXlzMBcs=";
   };
 
   build-system = [ pdm-backend ];
