ab2cc351e8072c57c4795918b9b3810c8e436a4e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-30 18:28:57 -0700

    nghttp3: 1.17.0 -> 1.18.0

--- pkgs/nghttp3/default.nix
+++ pkgs/nghttp3/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nghttp3";
-  version = "1.17.0";
+  version = "1.18.0";
 
   src = fetchurl {
     url = "https://github.com/ngtcp2/nghttp3/releases/download/v${finalAttrs.version}/nghttp3-${finalAttrs.version}.tar.bz2";
-    hash = "sha256-KobUa0Kx37XGLk/sO/5ugO5JVxXhiAqEaFdyiBDJgm0=";
+    hash = "sha256-yiMmaLlKXh+1S0FjLMVkc6tIImSDgUQQ72KnBilOZAI=";
   };
 
   outputs = [
