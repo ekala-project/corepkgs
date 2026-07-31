3750ed2e90218fbcd307bac31ed124c58bc6b7dc
Jonathan Ringer <jonringer117@gmail.com>
2026-07-30 18:06:01 -0700

    erlang: 27.3.4.6 -> 29.0.4

--- pkgs-many/erlang/variants.nix
+++ pkgs-many/erlang/variants.nix
@@ -4,8 +4,8 @@
     hash = "sha256-3WxURI1o/CqN0KK+voUMk6al2mtsrqoniBJpDxKpxPo=";
   };
   v27 = {
-    version = "27.3.4.6";
-    hash = "sha256-QC8C7rOpRXFriZtmVc48Se7IQ61p4DSAXVobnDeEkAE=";
+    version = "29.0.4";
+    hash = "sha256-Wq1aQ5a8IBJfY/N3f4AnfoSvodgdFo9lYEWpBy5YYTU=";
   };
   v28 = {
     version = "28.3";
