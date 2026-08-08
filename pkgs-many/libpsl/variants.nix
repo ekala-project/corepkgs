ddb80944d209c387a67ae9bfce2da6eceadcc9dc
Jonathan Ringer <jonringer117@gmail.com>
2026-08-07 19:16:04 -0700

    libpsl: 0.23.0 -> 0.23.1

--- pkgs-many/libpsl/variants.nix
+++ pkgs-many/libpsl/variants.nix
@@ -1,7 +1,7 @@
 {
   v0_21 = {
-    version = "0.23.0";
-    hash = "sha256-kGMeODqpfzTY6AqTb6Q3EzPFqQ8XwCVlGtWTRDH6xIA=";
+    version = "0.23.1";
+    hash = "sha256-k1iDgEd/77b78TE5op2oVAyMTA9PMoNey+xemaZ7dLE=";
   };
   minimal.isMinimalBuild = true;
 }
