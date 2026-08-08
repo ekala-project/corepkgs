880f1a337a246eacbb42d50a74cb91652d71ea69
Jonathan Ringer <jonringer117@gmail.com>
2026-08-07 18:16:03 -0700

    doctl: 1.165.0 -> 1.166.0

--- pkgs/doctl/default.nix
+++ pkgs/doctl/default.nix
@@ -9,13 +9,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "doctl";
-  version = "1.165.0";
+  version = "1.166.0";
 
   src = fetchFromGitHub {
     owner = "digitalocean";
     repo = "doctl";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-vwp7x7X6W38NQ4dAJv8ZTy0Q46oyk9AYxTs1gGwn9I4=";
+    hash = "sha256-m4KJ/WzmnLuL4/6RQV1sPUEc+PdKDNkTL2+HBGgN5x8=";
   };
 
   vendorHash = null;
