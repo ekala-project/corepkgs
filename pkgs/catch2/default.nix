03c4f7cbc706866b8810880842509c69709815db
Jonathan Ringer <jonringer117@gmail.com>
2026-08-07 18:05:44 -0700

    catch2: 3.15.2 -> 3.15.3

--- pkgs/catch2/default.nix
+++ pkgs/catch2/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "catch2";
-  version = "3.15.2";
+  version = "3.15.3";
 
   src = fetchFromGitHub {
     owner = "catchorg";
     repo = "Catch2";
     rev = "v${version}";
-    sha256 = "sha256-Fb8dnuaKQwLxYmGDZy38ZsCKk6RwE4PSidD1xmnb1rU=";
+    sha256 = "sha256-ZuH3tUWNklq0bKp0Yu9w3L5FNsQwUxe6lyGBv4M6U1E=";
   };
 
   nativeBuildInputs = [
