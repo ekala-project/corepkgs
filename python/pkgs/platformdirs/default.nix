e8925af36f3c6f70c67b84ddb17334a543b773a6
Jonathan Ringer <jonringer117@gmail.com>
2026-08-07 19:57:39 -0700

    python312Packages.platformdirs: 4.10.0 -> 4.11.1

--- python/pkgs/platformdirs/default.nix
+++ python/pkgs/platformdirs/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "platformdirs";
-  version = "4.10.0";
+  version = "4.11.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tox-dev";
     repo = "platformdirs";
     tag = version;
-    hash = "sha256-Sx5ln2mF2FkChP3UKu+GmOIIV8DNoJyYgsNiVDkVqQE=";
+    hash = "sha256-1HyrcQXtNTVAKRluwSV60uyKZHtjQ5lU2L0YcPxPuyk=";
   };
 
   build-system = [
