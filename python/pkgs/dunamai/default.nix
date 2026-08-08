f44aed89b6a6c68f0f8370069145a896b08da364
Jonathan Ringer <jonringer117@gmail.com>
2026-08-07 19:54:14 -0700

    python312Packages.dunamai: 1.26.1 -> 1.26.2

--- python/pkgs/dunamai/default.nix
+++ python/pkgs/dunamai/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "dunamai";
-  version = "1.26.1";
+  version = "1.26.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mtkennerly";
     repo = "dunamai";
     tag = "v${version}";
-    hash = "sha256-PKIJTkzKQ17wEp6AH/KldDyjtoGo0qU1RHzVaZCPLTU=";
+    hash = "sha256-pP0KeQ1tThvBomQIlrWNCVzB8ghR9rfaLm5NeBKwiuE=";
   };
 
   build-system = [ poetry-core ];
