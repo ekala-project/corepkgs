851e1d9f7e92ccca8780fcbeb7482f70176d39a2
Jonathan Ringer <jonringer117@gmail.com>
2026-08-16 23:32:46 -0700

    ekaos/modules: add configurable system.nssDatabases for nsswitch.conf
    Replaces the hardcoded /etc/nsswitch.conf in etc.nix with a dedicated
    nsswitch module providing system.nssDatabases options for passwd, group,
    shadow, hosts, and services. This allows modules like LDAP or mDNS to
    extend NSS lookup order declaratively.

---
+++ ekaos/modules/config/nsswitch.nix
@@ -0,0 +1,64 @@
+# Name Service Switch configuration (/etc/nsswitch.conf)
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+{
+  options.system.nssDatabases = {
+    passwd = mkOption {
+      type = types.listOf types.str;
+      default = [ "files" ];
+      description = "NSS passwd database entries for /etc/nsswitch.conf.";
+    };
+
+    group = mkOption {
+      type = types.listOf types.str;
+      default = [ "files" ];
+      description = "NSS group database entries for /etc/nsswitch.conf.";
+    };
+
+    shadow = mkOption {
+      type = types.listOf types.str;
+      default = [ "files" ];
+      description = "NSS shadow database entries for /etc/nsswitch.conf.";
+    };
+
+    hosts = mkOption {
+      type = types.listOf types.str;
+      default = [
+        "files"
+        "dns"
+      ];
+      description = "NSS hosts database entries for /etc/nsswitch.conf.";
+    };
+
+    services = mkOption {
+      type = types.listOf types.str;
+      default = [ "files" ];
+      description = "NSS services database entries for /etc/nsswitch.conf.";
+    };
+  };
+
+  config = {
+    environment.etc."nsswitch.conf".text = let
+      cfg = config.system.nssDatabases;
+    in ''
+      passwd:    ${concatStringsSep " " cfg.passwd}
+      group:     ${concatStringsSep " " cfg.group}
+      shadow:    ${concatStringsSep " " cfg.shadow}
+
+      hosts:     ${concatStringsSep " " cfg.hosts}
+      networks:  files
+
+      services:  ${concatStringsSep " " cfg.services}
+      protocols: files
+      rpc:       files
+      ethers:    files
+    '';
+  };
+}
