f74b373a38b67e7be642f8a5449d4f4769507fd8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:28:56 -0700

    libgudev: init at 238

---
+++ pkgs/libgudev/default.nix
@@ -0,0 +1,66 @@
+{
+  stdenv,
+  lib,
+  fetchurl,
+  pkg-config,
+  meson,
+  ninja,
+  udev,
+  glib,
+  vala,
+  gobject-introspection,
+  buildPackages,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libgudev";
+  version = "238";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/libgudev/${lib.versions.majorMinor finalAttrs.version}/libgudev-${finalAttrs.version}.tar.xz";
+    hash = "sha256-YSZqsa/J1z28YKiyr3PpnS/f9H2ZVE0IV2Dk+mZ7XdE=";
+  };
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    meson.configurePhaseHook
+    ninja
+    glib
+  ]
+  ++ lib.optionals withIntrospection [
+    gobject-introspection
+    vala
+  ];
+
+  buildInputs = [
+    udev
+    glib
+  ];
+
+  mesonFlags = [
+    (lib.mesonEnable "introspection" withIntrospection)
+    (lib.mesonEnable "vapi" withIntrospection)
+    (lib.mesonEnable "tests" false)
+  ];
+
+  mesonBuildType = "release";
+
+  meta = {
+    description = "Library that provides GObject bindings for libudev";
+    homepage = "https://gitlab.gnome.org/GNOME/libgudev";
+    license = lib.licenses.lgpl2Plus;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
