e2414b486d0f5fbe875d70259f7cadf67e185d19
Jonathan Ringer <jonringer117@gmail.com>
2026-08-23 05:28:52 -0700

    mobile-broadband-provider-info: init at 20240407

---
+++ pkgs/mobile-broadband-provider-info/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  libxslt,
+  libxml2,
+  meson,
+  ninja,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "mobile-broadband-provider-info";
+  version = "20240407";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${version}/${pname}-${version}.tar.xz";
+    hash = "sha256-ib/v8hX0v/jpw/8uwlJQ/bCA0R6b+lnG/HGYKsAcgUo=";
+  };
+
+  nativeBuildInputs = [
+    libxslt
+    libxml2
+    meson
+    meson.configurePhaseHook
+    ninja
+  ];
+
+  mesonBuildType = "release";
+
+  meta = {
+    description = "Mobile broadband service provider database";
+    homepage = "https://gitlab.gnome.org/GNOME/mobile-broadband-provider-info";
+    license = lib.licenses.publicDomain;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}
