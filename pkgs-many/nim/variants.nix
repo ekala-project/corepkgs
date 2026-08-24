9fe634a98091f943c58ceba7ca99de0a0f75efc7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 06:53:42 -0700

    nim: 2.2.0 -> 2.2.10

--- pkgs-many/nim/variants.nix
+++ pkgs-many/nim/variants.nix
@@ -1,7 +1,7 @@
 {
   v2_2 = {
-    version = "2.2.0";
+    version = "2.2.10";
     # Nim 2.2 (Latest stable)
-    src-hash = "sha256-zphChJyXYOSH7N0c2t98DyhEyvrmBUAcfHKuJXZEiTw=";
+    src-hash = "sha256-eVe37QBCBrzxC8xPO0dEFTh45i8kMVUqmo6dP0Do1dU=";
   };
 }
