6a26533f521cbc007e6585762852917892a9b734
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 06:54:38 -0700

    r-lang: add 4.6.1 variant, make it the default

--- pkgs-many/r-lang/variants.nix
+++ pkgs-many/r-lang/variants.nix
@@ -1,7 +1,13 @@
 {
   v4_4 = {
     version = "4.4.2";
-    # R 4.4 (Latest stable)
+    # R 4.4
     src-hash = "sha256-FXjNYD6NhmtYdD5J2L+ZxWnoEHm2pgzzPN973/64F+w=";
+  };
+
+  v4_6 = {
+    version = "4.6.1";
+    # R 4.6 (Latest stable)
+    src-hash = "sha256-TabmHSwKrF8UoufkMstfzCae/oPaQpMFC6fwPf9OLPQ=";
   };
 }
