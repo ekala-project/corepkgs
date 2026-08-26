9a963147d8f6d5982773d8024c325144036c0ab1
Jonathan Ringer <jonringer117@gmail.com>
2026-08-26 16:31:02 -0700

    potrace: init at 1.16

---
+++ pkgs/potrace/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  zlib,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "potrace";
+  version = "1.16";
+
+  src = fetchurl {
+    url = "https://potrace.sourceforge.net/download/${version}/potrace-${version}.tar.gz";
+    hash = "sha256-voJIoX3t1sy6qy/MRYNbsFAtBi5A+97TvFYCjOXresw=";
+  };
+
+  configureFlags = [
+    "--with-libpotrace"
+  ];
+
+  buildInputs = [
+    zlib
+  ];
+
+  meta = {
+    description = "Tool for tracing a bitmap into a smooth, scalable image";
+    homepage = "https://potrace.sourceforge.net/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ ];
+  };
+}
