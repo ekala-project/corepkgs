01bf6d85bebfd52a0c2dcdffd72d1127b99b4c02
Aliaksandr <grubian2@gmail.com>
2026-08-27 23:44:32 +0300

    prefetchNpmDeps: init

--- pkgs/prefetchNpmDeps/default.nix
+++ pkgs/prefetchNpmDeps/default.nix
@@ -1,7 +1,51 @@
-# prefetch-npm-deps: CLI tool for computing npm deps hash
-#
-# TODO: Build prefetch-npm-deps from source using the available Rust toolchain
-# instead of importing from nixpkgs.
-{ }:
+# prefetch-npm-deps: CLI tool for computing the npm dependency hash used by
+# `fetchNpmDeps`. The Rust sources are vendored here, as they are in nixpkgs.
+{
+  lib,
+  rustPlatform,
+  makeWrapper,
+  pkg-config,
+  curl,
+  gnutar,
+  gzip,
+}:
 
-throw "prefetchNpmDeps is not yet available in core-pkgs (needs to be built from source)"
+rustPlatform.buildRustPackage {
+  pname = "prefetch-npm-deps";
+  version = (lib.importTOML ./Cargo.toml).package.version;
+
+  src = lib.cleanSourceWith {
+    src = ./.;
+    filter =
+      name: _type:
+      let
+        base = baseNameOf name;
+      in
+      base != "default.nix" && base != "target";
+  };
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  nativeBuildInputs = [
+    makeWrapper
+    pkg-config
+  ];
+
+  buildInputs = [ curl ];
+
+  postInstall = ''
+    wrapProgram "$out/bin/prefetch-npm-deps" --prefix PATH : ${
+      lib.makeBinPath [
+        gnutar
+        gzip
+      ]
+    }
+  '';
+
+  meta = {
+    description = "Prefetch dependencies from npm (for use with `fetchNpmDeps`)";
+    mainProgram = "prefetch-npm-deps";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+  };
+}
