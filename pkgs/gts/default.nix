8a726fd7413ae79c4eeeb38ddb3ca7f257a749a7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-26 18:51:06 -0700

    gts: init at 0.7.6

---
+++ pkgs/gts/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkg-config,
+  autoreconfHook,
+  gettext,
+  glib,
+  buildPackages,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "gts";
+  version = "0.7.6";
+
+  outputs = [
+    "bin"
+    "dev"
+    "out"
+  ];
+
+  src = fetchurl {
+    url = "mirror://sourceforge/gts/gts-${version}.tar.gz";
+    sha256 = "07mqx09jxh8cv9753y2d2jsv7wp8vjmrd7zcfpbrddz3wc9kx705";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+    glib
+  ];
+  buildInputs = [ gettext ];
+  propagatedBuildInputs = [ glib ];
+
+  env.NIX_CFLAGS_COMPILE = "-std=gnu17";
+
+  preBuild = lib.optionalString (stdenv.hostPlatform != stdenv.buildPlatform) ''
+    pushd src
+    make CC=${buildPackages.stdenv.cc}/bin/cc predicates_init
+    mv predicates_init predicates_init_build
+    make clean
+    popd
+    substituteInPlace src/Makefile --replace "./predicates_init" "./predicates_init_build"
+  '';
+
+  meta = {
+    homepage = "https://gts.sourceforge.net/";
+    license = lib.licenses.lgpl2Plus;
+    description = "GNU Triangulated Surface Library";
+    maintainers = [ ];
+    platforms = lib.platforms.unix;
+  };
+}
