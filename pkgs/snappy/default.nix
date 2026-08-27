63246943d4fde291521ba07971c32ee17a80650b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-26 22:25:52 -0700

    snappy: init at 1.2.2

---
+++ pkgs/snappy/default.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchpatch,
+  cmake,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "snappy";
+  version = "1.2.2";
+
+  src = fetchFromGitHub {
+    owner = "google";
+    repo = "snappy";
+    rev = finalAttrs.version;
+    hash = "sha256-bMZD8EI9dvDGupfos4hi/0ShBkrJlI5Np9FxE6FfrNE=";
+  };
+
+  patches = [
+    ./revert-PUBLIC.patch
+    # Re-enable RTTI, without which other applications can't subclass snappy::Source
+    # https://tracker.ceph.com/issues/53060
+    # https://build.opensuse.org/package/show/openSUSE:Factory/snappy
+    (fetchpatch {
+      url = "https://build.opensuse.org/public/source/openSUSE:Factory/snappy/reenable-rtti.patch?rev=e3449869b466869fc6b8a03a1a528fa6";
+      hash = "sha256-JhVhkHh7XPx1Bzf5xnOgWLgwh1oihX3O+emQWzE4Dho=";
+    })
+  ];
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  cmakeBuildType = "Release";
+
+  cmakeFlags = [
+    "-DBUILD_SHARED_LIBS=ON"
+    "-DSNAPPY_BUILD_TESTS=OFF"
+    "-DSNAPPY_BUILD_BENCHMARKS=OFF"
+  ];
+
+  postInstall = ''
+    substituteInPlace "$out"/lib/cmake/Snappy/SnappyTargets.cmake \
+      --replace-fail 'INTERFACE_INCLUDE_DIRECTORIES "''${_IMPORT_PREFIX}/include"' 'INTERFACE_INCLUDE_DIRECTORIES "'$dev'"'
+
+    mkdir -p $dev/lib/pkgconfig
+    cat <<EOF > $dev/lib/pkgconfig/snappy.pc
+      Name: snappy
+      Description: Fast compressor/decompressor library.
+      Version: ${finalAttrs.version}
+      Libs: -L$out/lib -lsnappy
+      Cflags: -I$dev/include
+    EOF
+  '';
+
+  meta = {
+    description = "Compression/decompression library for very high speeds";
+    homepage = "https://google.github.io/snappy/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
