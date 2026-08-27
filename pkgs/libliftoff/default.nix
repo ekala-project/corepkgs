70643606478190dc02115d4b619de0fdf6346ca5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-27 08:57:48 -0700

    libliftoff: init at 0.5.0

---
+++ pkgs/libliftoff/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  meson,
+  ninja,
+  pkg-config,
+  libdrm,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libliftoff";
+  version = "0.5.0";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    owner = "emersion";
+    repo = "libliftoff";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-PcQY8OXPqfn8C30+GAYh0Z916ba5pik8U0fVpZtFb5g=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [ libdrm ];
+
+  mesonBuildType = "release";
+
+  meta = {
+    description = "Lightweight KMS plane library";
+    homepage = "https://gitlab.freedesktop.org/emersion/libliftoff";
+    changelog = "https://gitlab.freedesktop.org/emersion/libliftoff/-/tags/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
