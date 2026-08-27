655cea147e3561bc3a04015c452d26f03cdc7994
Jonathan Ringer <jonringer117@gmail.com>
2026-08-27 08:57:57 -0700

    libwacom: init at 2.19.1

---
+++ pkgs/libwacom/default.nix
@@ -0,0 +1,68 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  glib,
+  pkg-config,
+  udev,
+  libevdev,
+  libgudev,
+  python3,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libwacom";
+  version = "2.19.1";
+
+  outputs = [
+    "bin"
+    "out"
+    "dev"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "linuxwacom";
+    repo = "libwacom";
+    rev = "libwacom-${finalAttrs.version}";
+    hash = "sha256-BYfMltOBhb9iS2sTazibcdIaAq5WHecHJIHIfu/cUAQ=";
+  };
+
+  postPatch = ''
+    patchShebangs test/check-files-in-git.sh
+  '';
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    meson.configurePhaseHook
+    ninja
+    python3
+  ];
+
+  buildInputs = [
+    glib
+    udev
+    libevdev
+    libgudev
+  ];
+
+  mesonBuildType = "release";
+
+  mesonFlags = [
+    (lib.mesonEnable "tests" false)
+    (lib.mesonOption "sysconfdir" "/etc")
+  ];
+
+  doInstallCheck = true;
+
+  meta = {
+    description = "Libraries, configuration, and diagnostic tools for Wacom tablets running under Linux";
+    homepage = "https://linuxwacom.github.io/";
+    changelog = "https://github.com/linuxwacom/libwacom/blob/${finalAttrs.src.rev}/NEWS";
+    license = lib.licenses.hpnd;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
