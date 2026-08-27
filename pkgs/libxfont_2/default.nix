2b64fedaeb51fafff60bf3996323254ed0566ebd
Aliaksandr <grubian2@gmail.com>
2026-08-28 00:05:07 +0300

    libxfont_2: init at 2.0.9

---
+++ pkgs/libxfont_2/default.nix
@@ -0,0 +1,68 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  autoreconfHook,
+  pkg-config,
+  util-macros,
+  libfontenc,
+  xorgproto,
+  freetype,
+  xtrans,
+  zlib,
+  testers,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libxfont_2";
+  version = "2.0.9";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    group = "xorg";
+    owner = "lib";
+    repo = "libxfont";
+    tag = "libXfont2-${finalAttrs.version}";
+    hash = "sha256-yr3vtS7LCCHGLTns5NwJHExtY25hcYQfVsAiRro5F9w=";
+  };
+
+  strictDeps = true;
+
+  # No configure script in the git tree.
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    util-macros
+  ];
+
+  buildInputs = [
+    libfontenc
+    xorgproto
+    freetype
+    xtrans
+    zlib
+  ];
+
+  propagatedBuildInputs = [ xorgproto ];
+
+  passthru = {
+    tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+  };
+
+  meta = {
+    description = "X font handling library for server & utilities";
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libxfont";
+    license = with lib.licenses; [
+      mit
+      mitOpenGroup
+      hpnd
+      hpndSellVariant
+    ];
+    pkgConfigModules = [ "xfont2" ];
+    platforms = lib.platforms.unix;
+  };
+})
