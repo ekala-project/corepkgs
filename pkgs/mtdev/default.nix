b4e77cd998001412919e1fbd4f7b67309ea4b99c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-27 08:57:48 -0700

    mtdev: init at 1.1.7

---
+++ pkgs/mtdev/default.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "mtdev";
+  version = "1.1.7";
+
+  src = fetchurl {
+    url = "https://bitmath.org/code/mtdev/mtdev-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-oQetrSEB/srFSsf58OCg3RVdlUGT2lXCNAyX8v8dgU4=";
+  };
+
+  meta = {
+    description = "Multitouch Protocol Translation Library";
+    homepage = "https://bitmath.org/code/mtdev/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
