5fcfa9c8e1a89689c85e7a27b288cdc2aab5fcb6
Jonathan Ringer <jonringer117@gmail.com>
2026-08-31 16:04:40 -0700

    catch2_3: add include output

--- pkgs/catch2_3/default.nix
+++ pkgs/catch2_3/default.nix
@@ -12,6 +12,12 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "catch2";
   version = "3.15.3";
 
+  outputs = [
+    "out"
+    "include"
+  ];
+  outputInclude = "include";
+
   src = fetchFromGitHub {
     owner = "catchorg";
     repo = "Catch2";
