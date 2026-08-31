806acc6213b4c1dbca941dfda4577ca3c7fd8502
Jonathan Ringer <jonringer117@gmail.com>
2026-08-31 16:04:40 -0700

    taskflow: add include output

--- pkgs/taskflow/default.nix
+++ pkgs/taskflow/default.nix
@@ -11,6 +11,12 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "taskflow";
   version = "4.1.0";
 
+  outputs = [
+    "out"
+    "include"
+  ];
+  outputInclude = "include";
+
   src = fetchFromGitHub {
     owner = "taskflow";
     repo = "taskflow";
