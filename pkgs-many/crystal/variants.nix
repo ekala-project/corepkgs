e4162fc326098db8b58f7f31ea4ebc0c3a0ffe15
Jonathan Ringer <jonringer117@gmail.com>
2026-09-02 10:07:31 -0700

    crystal: migrate to mkManyVariants, add 1.21.0
    Move crystal from pkgs/ to pkgs-many/ using the mkManyVariants pattern.
    Add Crystal 1.21.0 (current) alongside the existing 1.20.3 release.
    Default to 1.21.0 as the current production version.

---
+++ pkgs-many/crystal/variants.nix
@@ -0,0 +1,13 @@
+{
+  v1_20 = rec {
+    version = "1.20.3";
+    src-url = "https://github.com/crystal-lang/crystal/releases/download/${version}/crystal-${version}-1-linux-x86_64.tar.gz";
+    src-hash = "sha256-ti6iWyR3E/tj3Jkr60WbytiVVGXcwRoYH9Ik1OyVWmI=";
+  };
+
+  v1_21 = rec {
+    version = "1.21.0";
+    src-url = "https://github.com/crystal-lang/crystal/releases/download/${version}/crystal-${version}-1-linux-x86_64.tar.gz";
+    src-hash = "sha256-dEVh7jzuGwbRBs+a6ZuAZLTgF1GKxBTW3SPPr+NlYMk=";
+  };
+}
