27c675c86811273191519fb2d1589b5c5be2048a
Jonathan Ringer <jonringer117@gmail.com>
2026-09-01 22:49:07 -0700

    go: add every patch version with major.minor aliases
    Expand variants.nix to include all patch releases for Go 1.24
    (1.24.0-1.24.13), 1.25 (1.25.0-1.25.14), and 1.26 (1.26.0-1.26.8).
    Add aliases so v1_24, v1_25, v1_26 resolve to the latest patch.

--- pkgs-many/go/variants.nix
+++ pkgs-many/go/variants.nix
@@ -1,5 +1,94 @@
 {
-  v1_24 = {
+  # Go 1.24.x
+  v1_24_0 = {
+    version = "1.24.0";
+    src-hash = "sha256-0UEgYUrLKdEryrcr1onyV+tL6eC2+IqPt+Qaxl+FVuU=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_1 = {
+    version = "1.24.1";
+    src-hash = "sha256-gkTr9GxlYH2xAiK1gGrrMcH8+JecG2sS9gxnfpo8BlY=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_2 = {
+    version = "1.24.2";
+    src-hash = "sha256-ncd/+twW2DehvzLZnGJMtN8GR87nsRnt2eexvMBfLgA=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_3 = {
+    version = "1.24.3";
+    src-hash = "sha256-IpwItgCxRGeYEJ+uH1aSKBAshHPKuoEEtkGMtbwDKHg=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_4 = {
+    version = "1.24.4";
+    src-hash = "sha256-WoaoOjH5+oFJC4xUIKw4T9PZWj5x+6Zlx7P5XR3+8rQ=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_5 = {
+    version = "1.24.5";
+    src-hash = "sha256-dP2wnyNS4rJbeUPlaDbJtHNj0o3sHItWxKlXDzC49Z8=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_6 = {
+    version = "1.24.6";
+    src-hash = "sha256-4ctVgqq1iGaLwEwH3hhogHD2uMmyqvNh+CHhm9R8/b0=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_7 = {
+    version = "1.24.7";
+    src-hash = "sha256-Ko9Q2w+IgDYHxQ1+qINNy3vUg8a0KKkeNg/fhiS0ZGQ=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_8 = {
+    version = "1.24.8";
+    src-hash = "sha256-sf8yxcSlDd+hoct4tg3Vo2KushhLt48Ai0JbYglXVfs=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_9 = {
+    version = "1.24.9";
+    src-hash = "sha256-xy+BulT+AO/n8+dJnUAJeSRogbE7d16am7hVQcEb5pU=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_10 = {
+    version = "1.24.10";
+    src-hash = "sha256-NAANzEelF7ePzyZX7n0DMyilcHn+YMTti3uEJg0dGdM=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_11 = {
     version = "1.24.11";
     src-hash = "sha256-/9+XdmpMSxNc1TgJcTl46e4alDssjiitIhpUKd4w4hA=";
     bootstrap = ./bootstrap/bootstrap122.nix;
@@ -7,7 +96,120 @@
     buildGoModuleSuffix = "124";
   };
 
-  v1_25 = {
+  v1_24_12 = {
+    version = "1.24.12";
+    src-hash = "sha256-+6LdZht757NNa9F+2S9BxEpeBZU62BqzS07HgOXn3EE=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  v1_24_13 = {
+    version = "1.24.13";
+    src-hash = "sha256-Y5piBMJIaxN98etueO4+0Dj5h30OS1pGXnlqIVP4WNc=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.23/iana-etc-1.17.patch;
+    buildGoModuleSuffix = "124";
+  };
+
+  # Go 1.25.x
+  v1_25_0 = {
+    version = "1.25.0";
+    src-hash = "sha256-S9AekSlyB7+kUOpA1NWpOxtTGl5DhHOyoG4Y4HciciU=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_1 = {
+    version = "1.25.1";
+    src-hash = "sha256-0BDBCc7pTYDv5oHqtGvepJGskGv0ZYPDLp8NuwvRpZQ=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_2 = {
+    version = "1.25.2";
+    src-hash = "sha256-NxEUDPuH/Oj3oT982GDfBB5sEvdhD0DKxuxvorZeluQ=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_3 = {
+    version = "1.25.3";
+    src-hash = "sha256-qBpLpZPQAV4QxR4mfeP/B8eskU38oDfZUX0ClRcJd5U=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_4 = {
+    version = "1.25.4";
+    src-hash = "sha256-FgBDt/F7bWC1A2lDaRf9qNUDRkC6Oa4kMca5WoicyYw=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_5 = {
+    version = "1.25.5";
+    src-hash = "sha256-IqX9CpHvzSihsFNxBrmVmygEth9Zw3WLUejlQpwalU8=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_6 = {
+    version = "1.25.6";
+    src-hash = "sha256-WMv3ceRNdt5vVtGeM7d9dFoeSJNAkih15GWFuXXCsFk=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_7 = {
+    version = "1.25.7";
+    src-hash = "sha256-F48oMoICdLQ+F30y8Go+uwEp5CfdIKXkyI3ywXY88Qo=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_8 = {
+    version = "1.25.8";
+    src-hash = "sha256-6YjUokRqx/4/baoImljpk2pSo4E1Wt7ByJgyMKjWxZ4=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_9 = {
+    version = "1.25.9";
+    src-hash = "sha256-DsnvjrzqCXqsN97K6fCachi0Uc2Wvn1u1RPY5Lz5Cc8=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_10 = {
+    version = "1.25.10";
+    src-hash = "sha256-IM8EqS5a+ZdI40G8iZb6KAkMmsmHZfoRXsXd9B169B0=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_11 = {
+    version = "1.25.11";
+    src-hash = "sha256-e05bB5s8m8QgNzymhiGilrTRPBBzXUrKxBcZKNcPVIA=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_12 = {
     version = "1.25.12";
     src-hash = "sha256-+Q3O5L0CP6N2N06gpabr5VNTeznEJv/YxolGm0VRmTI=";
     bootstrap = ./bootstrap/bootstrap122.nix;
@@ -15,9 +217,98 @@
     buildGoModuleSuffix = "125";
   };
 
-  v1_26 = {
+  v1_25_13 = {
+    version = "1.25.13";
+    src-hash = "sha256-HX4vcLHum5PH3478ynH1rcxqWXl6QzbC0QFxvUwXRhQ=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  v1_25_14 = {
+    version = "1.25.14";
+    src-hash = "sha256-noP0T1/ClzeIYbThbMaqEUvordeZP7POssUSOAqk1YI=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "125";
+  };
+
+  # Go 1.26.x
+  v1_26_0 = {
+    version = "1.26.0";
+    src-hash = "sha256-yRMqih9r0qpKrR10uCMdlSdJUEg6SVBlfubFbm6Bd5A=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_1 = {
+    version = "1.26.1";
+    src-hash = "sha256-MXIpPQSyCdwRRGmOe6E/BHf2uoxf/QvmbCD9vJeF37s=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_2 = {
+    version = "1.26.2";
+    src-hash = "sha256-LpHrtpR6lulDb7KzkmqIAu/mOm03Xf/sT4Kqnb1v1Ds=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_3 = {
+    version = "1.26.3";
+    src-hash = "sha256-HGRoddCqh5kTMYTtV895/yS97+jIggRwYCqdPW2Rkrg=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_4 = {
+    version = "1.26.4";
+    src-hash = "sha256-T2aKMvv8ETLmqIH7lowvHa2mMUkqM5IRc1+7JVpCYC0=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_5 = {
     version = "1.26.5";
     src-hash = "sha256-SVvkvIcXasVnOS5bQRar2YRm0z17SdQedkzMaXay3EI=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_6 = {
+    version = "1.26.6";
+    src-hash = "sha256-oHIcVMaIkBRI13rZs+x+p8R0cwdV/4kTgukuy5P/LLE=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_7 = {
+    version = "1.26.7";
+    src-hash = "sha256-DtJOrHVRBQhbif6cq8J0K5GbrXuUtZ0602SRjryJVq0=";
+    bootstrap = ./bootstrap/bootstrap122.nix;
+    iana-patch = ./patches/1.25/iana-etc-1.25.patch;
+    buildGoModuleSuffix = "126";
+    bootstrapGo = buildPackages: buildPackages.go.v1_25;
+  };
+
+  v1_26_8 = {
+    version = "1.26.8";
+    src-hash = "sha256-Tjm5jkL5RvoFrIvFtxh335fb23y7Gnd7VBZnrXEX/S4=";
     bootstrap = ./bootstrap/bootstrap122.nix;
     iana-patch = ./patches/1.25/iana-etc-1.25.patch;
     buildGoModuleSuffix = "126";
