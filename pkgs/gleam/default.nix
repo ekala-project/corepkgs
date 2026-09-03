fd06753da0ca8d49a7ec0571d03602595b780d52
Jonathan Ringer <jonringer117@gmail.com>
2026-09-03 12:19:39 -0700

    gleam: init at 1.18.1

---
+++ pkgs/gleam/default.nix
@@ -0,0 +1,34 @@
+# gleam — Statically typed language for the Erlang VM
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  erlang,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "gleam";
+  version = "1.18.1";
+
+  src = fetchFromGitHub {
+    owner = "gleam-lang";
+    repo = "gleam";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-974B+22Lvd7KB9M0yuuxkolLtRmg42NrAX5CIrIc3Ac=";
+  };
+
+  cargoHash = "sha256-as+2oyOpGA71oPDGTuZhfPccr8AjsUZJFtnRLYRxFOI=";
+
+  nativeBuildInputs = [
+    pkg-config
+    erlang
+  ];
+
+  meta = {
+    description = "Statically typed language for the Erlang VM";
+    homepage = "https://gleam.run/";
+    license = lib.licenses.asl20;
+    mainProgram = "gleam";
+  };
+})
