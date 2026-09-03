a74b4c4b2fc568b85248489af1f2a75a4771a500
Jonathan Ringer <jonringer117@gmail.com>
2026-09-03 12:07:45 -0700

    ekaos/languages: add javascript module

---
+++ ekaos/modules/languages/javascript.nix
@@ -0,0 +1,22 @@
+# JavaScript programming language module
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  langLib = import ./lib.nix { inherit lib; };
+  mod = langLib.mkLanguageModule {
+    name = "javascript";
+    defaultPackage = pkgs: pkgs.nodejs;
+    resolveVersion = langLib.mkMajorVersionResolver "nodejs";
+    environmentVariables = _: {
+      NODE_PATH = "$HOME/.node_modules";
+    };
+    sessionPath = _: [ "$HOME/.node_modules/.bin" ];
+  };
+in
+
+mod { inherit config lib pkgs; }
