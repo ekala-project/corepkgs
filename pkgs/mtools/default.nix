0aa0e5c3183430eb788602ba360cb3dddd5b2d47
Jonathan Ringer <jonringer117@gmail.com>
2026-09-16 06:05:59 -0700

    mtools, libburn, libisofs, libcdio, libcddb, libisoburn: init
    Port ISO image toolchain: mtools (FAT filesystem tools), libisoburn
    (xorriso ISO creator), and their dependency chain (libburn, libisofs,
    libcdio, libcddb). Required for building EkaOS installation media.

---
+++ pkgs/mtools/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "mtools";
+  version = "4.0.49";
+
+  src = fetchurl {
+    url = "mirror://gnu/mtools/mtools-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-b+UZNYPW58Wdp15j1yNPdsCwfK8zsQOJT0b2aocf/J8=";
+  };
+
+  enableParallelBuilding = true;
+
+  meta = {
+    homepage = "https://www.gnu.org/software/mtools/";
+    description = "Utilities to access MS-DOS disks";
+    platforms = lib.platforms.unix;
+    license = lib.licenses.gpl3;
+  };
+})
