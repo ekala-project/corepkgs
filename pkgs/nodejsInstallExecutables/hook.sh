b83d159b5384e23680009dbd24b993ad315b19e2
Jonathan Ringer <jonringer117@gmail.com>
2026-09-17 09:05:31 -0700

    python, nodejs: remove string fallback for makeWrapperArgs
    With __structuredAttrs always enabled, makeWrapperArgs is always an
    array. Remove the IFS-separated string detection branch.

--- pkgs/nodejsInstallExecutables/hook.sh
+++ pkgs/nodejsInstallExecutables/hook.sh
@@ -5,16 +5,7 @@ nodejsInstallExecutables() {
 
     local -r packageOut="$out/lib/node_modules/$(@jq@ --raw-output '.name' package.json)"
 
-    # Based on code from Python's buildPythonPackage wrap.sh script, for
-    # supporting both the case when makeWrapperArgs is an array and a
-    # IFS-separated string.
-    #
-    # TODO: remove the string branch when __structuredAttrs are used.
-    if [[ "${makeWrapperArgs+defined}" == "defined" && "$(declare -p makeWrapperArgs)" =~ ^'declare -a makeWrapperArgs=' ]]; then
-        local -a user_args=("${makeWrapperArgs[@]}")
-    else
-        local -a user_args="(${makeWrapperArgs:-})"
-    fi
+    local -a user_args=(${makeWrapperArgs+"${makeWrapperArgs[@]}"})
 
     while IFS=" " read -ra bin; do
         mkdir -p "$out/bin"
