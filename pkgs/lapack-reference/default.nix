2b4a03a7404f450e4f5612e5f5090b570da85106
Jonathan Ringer <jonringer117@gmail.com>
2026-09-18 00:20:47 -0700

    lapack-reference: migrate cmakeFlags to cmakeEntries

--- pkgs/lapack-reference/default.nix
+++ pkgs/lapack-reference/default.nix
@@ -33,20 +33,19 @@ stdenv.mkDerivation (finalAttrs: {
     "stackprotector"
   ];
 
-  cmakeFlags = [
-    "-DCMAKE_Fortran_FLAGS=-fPIC"
-    "-DLAPACKE=ON"
-    "-DCBLAS=ON"
-    "-DBUILD_TESTING=ON"
-  ]
-  ++ lib.optional shared "-DBUILD_SHARED_LIBS=ON"
-  ++ lib.optional blas64 "-DBUILD_INDEX64=ON"
-  # Tries to run host platform binaries during the build
-  # Will likely be disabled by default in 3.12, see:
-  # https://github.com/Reference-LAPACK/lapack/issues/757
-  ++ lib.optional (
-    !stdenv.buildPlatform.canExecute stdenv.hostPlatform
-  ) "-DTEST_FORTRAN_COMPILER=OFF";
+  cmakeEntries = {
+    CMAKE_Fortran_FLAGS = "-fPIC";
+    LAPACKE = "ON";
+    CBLAS = "ON";
+    BUILD_TESTING = "ON";
+    ${if shared then "BUILD_SHARED_LIBS" else null} = "ON";
+    ${if blas64 then "BUILD_INDEX64" else null} = "ON";
+    # Tries to run host platform binaries during the build
+    # Will likely be disabled by default in 3.12, see:
+    # https://github.com/Reference-LAPACK/lapack/issues/757
+    ${if !stdenv.buildPlatform.canExecute stdenv.hostPlatform then "TEST_FORTRAN_COMPILER" else null} =
+      "OFF";
+  };
 
   passthru = { inherit blas64; };
 
