503cefa896459fd6ffbc0f96e1a8967fed83ec43
Jonathan Ringer <jonringer117@gmail.com>
2026-09-18 05:34:12 -0700

    graphene: init at 1.10.8

---
+++ pkgs/graphene/default.nix
@@ -0,0 +1,74 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  fetchpatch,
+  pkg-config,
+  meson,
+  ninja,
+  python3,
+  glib,
+  gobject-introspection,
+  buildPackages,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "graphene";
+  version = "1.10.8";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "ebassi";
+    repo = "graphene";
+    rev = finalAttrs.version;
+    sha256 = "P6JQhSktzvyMHatP/iojNGXPmcsxsFxdYerXzS23ojI=";
+  };
+
+  depsBuildBuild = [
+    pkg-config
+  ];
+
+  nativeBuildInputs = [
+    meson
+    meson.configurePhaseHook
+    ninja
+    pkg-config
+    python3
+  ]
+  ++ lib.optionals withIntrospection [
+    gobject-introspection
+  ];
+
+  buildInputs = [
+    glib
+  ];
+
+  mesonFlags = [
+    (lib.mesonBool "gtk_doc" false)
+    (lib.mesonEnable "introspection" withIntrospection)
+    (lib.mesonBool "installed_tests" false)
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isAarch32 [
+    "-Darm_neon=false"
+  ];
+
+  doCheck = true;
+
+  postPatch = ''
+    patchShebangs tests/gen-installed-test.py
+  '';
+
+  meta = {
+    description = "Thin layer of graphic data types";
+    homepage = "https://github.com/ebassi/graphene";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+  };
+})
