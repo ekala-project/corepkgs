99bee65ced62779f7ae1e4b3ba7cb64165b0892e
Jonathan Ringer <jonringer117@gmail.com>
2026-09-18 00:11:50 -0700

    aws-c-s3: migrate cmakeFlags to cmakeEntries

--- pkgs/aws-c-s3/default.nix
+++ pkgs/aws-c-s3/default.nix
@@ -48,9 +48,9 @@ stdenv.mkDerivation rec {
     s2n-tls
   ];
 
-  cmakeFlags = [
-    "-DBUILD_SHARED_LIBS=ON"
-  ];
+  cmakeEntries = {
+    BUILD_SHARED_LIBS = true;
+  };
 
   passthru.tests = {
     inherit nix;
