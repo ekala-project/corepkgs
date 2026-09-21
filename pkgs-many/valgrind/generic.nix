6842c36e8f4d4f266c119d22449564028371b0f6
Jonathan Ringer <jonringer117@gmail.com>
2026-09-21 16:50:55 -0700

    valgrind: init at 3.27.1

---
+++ pkgs-many/valgrind/generic.nix
@@ -0,0 +1,118 @@
+{
+  version,
+  src-hash,
+  withGdb ? true,
+  ...
+}:
+
+{
+  lib,
+  stdenv,
+  fetchurl,
+  fetchpatch,
+  autoreconfHook,
+  perl,
+  # TODO: gdb is not yet available in core-pkgs
+  gdb ? null,
+}:
+
+stdenv.mkDerivation {
+  pname = "valgrind";
+  inherit version;
+
+  src = fetchurl {
+    url = "https://sourceware.org/pub/valgrind/valgrind-${version}.tar.bz2";
+    hash = src-hash;
+  };
+
+  patches = [
+    # Fix checks on Musl.
+    # https://bugs.kde.org/show_bug.cgi?id=453929
+    (fetchpatch {
+      url = "https://bugsfiles.kde.org/attachment.cgi?id=148912";
+      sha256 = "Za+7K93pgnuEUQ+jDItEzWlN0izhbynX2crSOXBBY/I=";
+    })
+    # https://bugs.kde.org/show_bug.cgi?id=511548
+    (fetchpatch {
+      url = "https://bugsfiles.kde.org/attachment.cgi?id=186451";
+      hash = "sha256-IGmyHwwGoy00hcz3XxQSDcwcU8zHLBJ9dfqTvWDQ520=";
+    })
+    (fetchpatch {
+      name = "reallocarray-test-musl.patch";
+      url = "https://sourceware.org/git/?p=valgrind.git;a=patch;h=991961ece87e4cdc0771a05c956c55baa437bb07";
+      hash = "sha256-U16384rLXMhLE5Em9z8FKYbshPlnq8l9ejC2+epL7M4=";
+    })
+
+    # Fix build on armv7l.
+    # see also https://bugs.kde.org/show_bug.cgi?id=454346
+    (fetchpatch {
+      url = "https://git.yoctoproject.org/poky/plain/meta/recipes-devtools/valgrind/valgrind/use-appropriate-march-mcpu-mfpu-for-ARM-test-apps.patch?id=b7a9250590a16f1bdc8c7b563da428df814d4292";
+      sha256 = "sha256-sBZzn98Sf/ETFv8ubivgA6Y6fBNcyR8beB3ICDAyAH0=";
+    })
+  ];
+
+  outputs = [
+    "out"
+    "dev"
+    "man"
+    "doc"
+  ];
+
+  hardeningDisable = [
+    "stackprotector"
+  ];
+
+  # GDB is needed to provide a sane default for `--db-command'.
+  # Perl is needed for `callgrind_{annotate,control}'.
+  buildInputs = lib.optional (withGdb && gdb != null) gdb ++ [
+    perl
+  ];
+
+  # Perl is also a native build input.
+  nativeBuildInputs = [
+    autoreconfHook
+    perl
+  ];
+
+  enableParallelBuilding = true;
+  separateDebugInfo = stdenv.hostPlatform.isLinux;
+
+  preConfigure = lib.optionalString stdenv.hostPlatform.isFreeBSD ''
+    substituteInPlace configure --replace-fail '`uname -r`' ${stdenv.cc.libc.version}-
+  '';
+
+  configureFlags = lib.optional stdenv.hostPlatform.isx86_64 "--enable-only64bit";
+
+  postInstall = ''
+    for i in $out/libexec/valgrind/*.supp; do
+      substituteInPlace $i \
+        --replace 'obj:/lib' 'obj:*/lib' \
+        --replace 'obj:/usr/X11R6/lib' 'obj:*/lib' \
+        --replace 'obj:/usr/lib' 'obj:*/lib'
+    done
+  '';
+
+  meta = {
+    homepage = "https://valgrind.org/";
+    description = "Debugging and profiling tool suite" + lib.optionalString (!withGdb) " (without GDB)";
+
+    longDescription = ''
+      Valgrind is an award-winning instrumentation framework for
+      building dynamic analysis tools.  There are Valgrind tools that
+      can automatically detect many memory management and threading
+      bugs, and profile your programs in detail.  You can also use
+      Valgrind to build new tools.
+    '';
+
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "valgrind";
+
+    platforms =
+      with lib.platforms;
+      lib.intersectLists (x86 ++ power ++ s390x ++ armv7 ++ aarch64 ++ mips ++ riscv64) (
+        darwin ++ freebsd ++ illumos ++ linux
+      );
+    badPlatforms = [ lib.systems.inspect.platformPatterns.isStatic ];
+    broken = stdenv.hostPlatform.isDarwin;
+  };
+}
